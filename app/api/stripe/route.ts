9420641572e63ea43a8ed49d3212eaa3be246ea5
Akansha <akanshapandey882004@gmail.com>
2025-01-09 20:13:09 +0530

    url changed

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -4,7 +4,7 @@ import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
 import { absoluteUrl } from "@/lib/utils";
 
-const settingsUrl = absoluteUrl("/settings");
+const settingsUrl ="https://genix-ai-saas.vercel.app/settings";
 
 export async function GET(req: Request) {
   try {
